251f2b6e50e8472807201bde0226ffb418561e2d
TheAlot <viktorvase@gmail.com>
2018-12-29 14:32:16 +0100

    Initial commit

---
+++ 3dApp/Assets/Scripts/RecieveFromSimon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecieveFromSimon : MonoBehaviour {
+
+	void GetString(string hej)
+    {
+        gameObject.GetComponent<Text>().text = hej;
+    }
+}
